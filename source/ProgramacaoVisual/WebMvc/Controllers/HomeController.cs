d774ae22f20ccb245bd1c7101e0742535dee9857
FelipeScatolin <fil.l@live.com>
2018-02-28 01:30:16 +0000

    Initial commit

---
+++ source/ProgramacaoVisual/WebMvc/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static IList<Pessoa> PessoaList = new List<Pessoa>
+        {
+            new Pessoa { Id = 1, Nome = "Joao" },
+            new Pessoa { Id = 1, Nome = "Juan" },
+            new Pessoa { Id = 1, Nome = "Jhon" }
+        };
+
+        public IList<Pessoa> GetPessoas()
+        {
+            return PessoaList;
+        }
+
+public Pessoa AddPessoa(String nome)
+{
+    try {
+        var pessoa = new Pessoa { Nome = nome};
+        pessoa.Id = PessoaList.Max(p => p.Id) +1;
+        PessoaList.Add(pessoa);
+        return pessoa;
+    } catch {}
+
+    return null;
+}
+           public IActionResult Index()
+{
+    return View();
+}
+
+public IActionResult About()
+{
+    ViewData["Message"] = "Your application description page.";
+
+    return View();
+}
+
+public IActionResult Contact()
+{
+    ViewData["Message"] = "Your contact page.";
+
+    return View();
+}
+
+public IActionResult Error()
+{
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+}
+    }
+}
