66fd93a836288d2b862d80c822fe1a8a354daa80
FelipeScatolin <fil.l@live.com>
2018-03-14 01:11:53 +0000

    Initial commit

--- source/ProgramacaoVisual/WebMvc/Controllers/PessoasController.cs
+++ source/ProgramacaoVisual/WebMvc/Controllers/PessoasController.cs
@@ -27,5 +27,23 @@ namespace WebMvc.Controllers
 
             return View(viewModel);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Nome")]Pessoa newPessoa)
+        {
+            if (!ModelState.IsValid)
+            return View(newPessoa);
+
+            newPessoa.Id = pessoaList.Max(p => p.Id) + 1;
+            pessoaList.Add(newPessoa);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
